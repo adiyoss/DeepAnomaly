dcc13d9001dfc8f7ba67208c308d59b8296cf2b4
danny <danny@karmon.org>
2016-06-10 18:00:23 +0300

    added encoder code for dimension reduction phase

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ from utils import create_sequences
 input_size = 1
 hidden_size = 128
 dropout_rate = 0.2
-maxlen = 20
+maxlen = 140
 batch_size = 32
 early_stopping_patience = 5
 save_dir = 'results/'
@@ -18,36 +18,37 @@ val_percentage = 0.15
 
 path_test = 'data/ECG5000_TEST_CONTINUOUS_SIGNAL_1.pkl'
 db = reader.read_data(path_test)
-X = create_sequences(db[:-1], win_size=maxlen, step=maxlen)
+X = create_sequences(db[:-140], win_size=maxlen, step=maxlen)
 
 X = np.reshape(X, (X.shape[0], X.shape[1], input_size))
 
 # build the model: 1 layer LSTM
 print('Build model...')
 model = Sequential()
-model.add(LSTM(hidden_size, return_sequences=True, input_shape=(maxlen, input_size)))
-model.add(Dropout(dropout_rate))
-model.add(TimeDistributed(Dense(input_size)))
+model.add(LSTM(hidden_size, return_sequences=False, input_shape=(maxlen, input_size)))
+model.add(Dense(140))
+# model.add(Dropout(dropout_rate))
+# model.add(LSTM(hidden_size, return_sequences=False))
+# model.add(Dense(input_size))
 
 model.load_weights(save_dir + model_name)
 model.compile(loss='mse', optimizer='adam')
 
-prediction_container = []
-value = X[0][0]
-# value = np.reshape(value, (1, value.shape[0], value.shape[1]))
-value = np.reshape(value, (1, 1, 1))
+# prediction_container = []
+# value = X[0][0]
+# # value = np.reshape(value, (1, value.shape[0], value.shape[1]))
+# value = np.reshape(value, (1, 1, 1))
 
-for i in range(1000):
-    prediction = model.predict(value, verbose=1)
-    value = prediction
-    prediction_container.append(prediction[0])
+# for i in range(1000):
+#     prediction = model.predict(value, verbose=1)
+#     value = prediction
+#     prediction_container.append(prediction[0])
 
-# prediction = model.predict(X, batch_size, verbose=1)
-# prediction = prediction.flatten()
-prediction_container = np.array(prediction_container).flatten()
-Y = db[1:len(prediction_container)]
-plt.plot(prediction_container, label='prediction')
+prediction = model.predict(X, batch_size, verbose=1)
+prediction = prediction.flatten()
+# prediction_container = np.array(prediction).flatten()
+Y = db[140:]
+plt.plot(prediction, label='prediction')
 plt.plot(Y, label='true')
 plt.legend()
 plt.show()
-
